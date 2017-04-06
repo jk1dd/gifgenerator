15d66ae054ac1c377e471c1521d775d32485441e
jk1dd <portajonk@gmail.com>
2017-04-06 15:56:34 -0600

    user can create account test passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create, :show] do
+    resources :favorites, only: [:index]
+  end
+
+  namespace :admin do
+    root to: 'admin/categories#index'
+    resources :categories do
+      resources :gifs
+    end
+  end
+
+  resources :gifs, only: [:index, :show]
+
 end
--- spec/features/users/user_can_create_account_spec.rb
+++ spec/features/users/user_can_create_account_spec.rb
@@ -8,6 +8,7 @@ RSpec.feature "User can create an account" do
       fill_in "user[username]", with: "jk"
       fill_in "user[email]", with: "sample@sample.com"
       fill_in "user[password]", with: "password"
+      fill_in "user[password_confirmation]", with: "password"
       click_on "Create Account"
 
       expect(current_path).to eq(user_path(User.last))
@@ -23,10 +24,12 @@ RSpec.feature "User can create an account" do
       fill_in "user[username]", with: "jk"
       fill_in "user[email]", with: ""
       fill_in "user[password]", with: "password"
+      fill_in "user[password_confirmation]", with: "password"
       click_on "Create Account"
 
       expect(current_path).to eq(new_user_path)
-      expect(page).to have_content("Retry login!")
+      expect(page).to have_content("Retry sign-up!")
+      expect(page).to have_content("Username")
       expect(page).to_not have_content("Welcome, jk!")
     end
   end
