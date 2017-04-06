d85aa5ca6712e113bb8d83e87a8531fabaafcad3
jk1dd <portajonk@gmail.com>
2017-04-06 17:17:28 -0600

    log out testing complete

--- spec/features/users/user_can_log_out_spec.rb
+++ spec/features/users/user_can_log_out_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 RSpec.feature "User can log out" do
   scenario "they can log out" do
-    user = User.create(username: "steve", password: "pass")
+    user = User.create(username: "steve", email: "s@s.com", password: "pass")
 
     visit login_path
-    fill_in "user[name]", with: "steve"
-    fill_in "user[password]", with: "pass"
-    click_on "Login"
+    fill_in "session[username]", with: "steve"
+    fill_in "session[password]", with: "pass"
+    click_on "Log In"
+
+    expect(current_path).to eq(user_path(user))
 
     click_on "Logout"
-    expect(current_path).to eq(root)
+    expect(current_path).to eq(root_path)
     expect(page).to have_content("Logout successful!")
-    expect(page).to have_content("Login")
+    expect(page).to have_button("Log In")
   end
 end
