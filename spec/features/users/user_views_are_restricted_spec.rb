e89994b8f0248022b8db04969467c743d889d30f
jk1dd <portajonk@gmail.com>
2017-04-06 17:36:51 -0600

    user roles added

--- spec/features/users/user_views_are_restricted_spec.rb
+++ spec/features/users/user_views_are_restricted_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
-
-RSpec.feature " gifs view" do
-  context "as admin"
-end
+# require 'rails_helper'
+#
+# RSpec.feature " gifs view" do
+#   context "as admin"
+# end
 # what do we want to test here?
