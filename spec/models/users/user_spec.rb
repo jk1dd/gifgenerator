7551665fdc282d7dbde4e297c57b2a28c70a0506
jk1dd <portajonk@gmail.com>
2017-04-07 08:12:56 -0600

    add attribute validation tests to user

--- spec/models/users/user_spec.rb
+++ spec/models/users/user_spec.rb
@@ -1,23 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  it "can be created as admin" do
-    user = User.create(username: "steve",
-                       email: "test",
-                       password: "pass",
-                       role: 1)
-    expect(user.role).to eq("admin")
-    expect(user.admin?).to be_truthy
-    expect(user.default?).to be_falsey
+  context "roles" do
+    it "can be created as admin" do
+      user = User.create(username: "steve",
+      email: "test",
+      password: "pass",
+      role: 1)
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+      expect(user.default?).to be_falsey
+    end
+
+    it "can be created as default" do
+      user = User.create(username: "steve",
+      email: "test",
+      password: "pass",
+      role: 0)
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+      expect(user.admin?).to be_falsey
+    end
   end
 
-  it "can be created as default" do
-    user = User.create(username: "steve",
-                       email: "test",
-                       password: "pass",
-                       role: 0)
-    expect(user.role).to eq("default")
-    expect(user.default?).to be_truthy
-    expect(user.admin?).to be_falsey
+  context "attributes" do
+    it "is valid with all attributes" do
+      user = User.create(username: "steve",
+      email: "test",
+      password: "pass")
+
+      expect(user).to be_valid
+    end
+
+    it "is invalid with missing attribs" do
+      user = User.create(username: "steve",
+      email: "test",
+      password: "")
+
+      expect(user).to_not be_valid
+    end
   end
 end
