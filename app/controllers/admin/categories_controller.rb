65dbd4264e8a75f4e7b96eaf2b8b37e3dcd2b2b0
jk1dd <portajonk@gmail.com>
2017-04-08 14:21:18 -0600

    WIP add create gif to admin categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,16 +6,19 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def create
+    @name = params[:category][:name]
+    response = Faraday.get("http://api.giphy.com/v1/gifs/search?q=#{@name}&api_key=dc6zaTOxFJmzC")
+    raw_data = response.body
+    data = JSON.parse(raw_data)
+    url = data["data"][1]["embed_url"]
     @category = Category.new(category_params)
     if @category.save
+      @category.gifs.create(image_url: url)
       redirect_to category_path(@category)
     else
       render :new
     end
-    # response = Faraday.get('http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC')
-    # raw_data = response.body
-    # data = JSON.parse(raw_data)
-    # url = data["data"][1]["embed_url"]
+    # response = Faraday.get("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC")
     # # Category.gifs.create(image_url: url)
     # GIF.create(image_url: url)
 
