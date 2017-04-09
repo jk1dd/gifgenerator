c0dc027c3e1d3d7c97941ba03b5bfd72d0b3541d
jk1dd <portajonk@gmail.com>
2017-04-09 15:25:58 -0600

    add tests for add and remove favorites

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -4,12 +4,15 @@ class GifsController < ApplicationController
     type = params[:type]
     if type == "favorite"
       current_user.gifs << @gif unless current_user.gifs.include?(@gif)
-      redirect_to :back, notice: "You favorited this gif"
+      flash[:notice] = "Successfully added to favorites!"
+      redirect_back(fallback_location: category_path(@gif.category))
     elsif type == "unfavorite"
       current_user.gifs.delete(@gif)
-      redirect_to :back, notice: "You removed this gif from favorites"
+      flash[:notice] = "Successfully removed from favorites!"
+      redirect_back(fallback_location: user_path(current_user))
     else
-      redirect_to :back, notice: "Nothing happened."
+      redirect_back(fallback_location: category_path(@gif.category))
+      flash[:notice] = "Nothing happened."
     end
   end
 end
--- spec/features/users/user_can_remove_favorite_spec.rb
+++ spec/features/users/user_can_remove_favorite_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "user visits category show page" do
-  context "user can favorite gif" do
-    it "gif appears on user show page" do
+RSpec.describe "user visits user show page" do
+  context "user can unfavorite gif" do
+    it "gif appears on user show page where user can unfavorite it" do
       category = Category.create(name: "water")
       gif = Gif.create(image_url: "http://giphy.com/embed/11sBLVxNs7v6WA", category_id: 1 )
       user = User.create(username: "steve",
@@ -24,6 +24,13 @@ RSpec.describe "user visits category show page" do
       expect(current_path).to eq(user_path(user))
       expect(page).to have_css("iframe")
       expect(user.favorites.count).to eq(1)
+
+      click_on "Remove from Favorites"
+
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_content("Successfully removed from favorites!")
+      expect(page).to_not have_css("iframe")
+      expect(user.favorites.count).to eq(0)
     end
   end
 end
